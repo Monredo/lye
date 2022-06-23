8d56f70316a966acb182b441a0f4e00d6b24bd43
Monredo <107286254+Monredo@users.noreply.github.com>
2022-06-23 01:08:26 -0400

    Update and rename changefont.js to script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function changeFont(font) {
   // body
   document.getElementById('content').style.fontFamily=font.value;
 }
+
+function changeSize(n){
+  var s=document.getElementById('content');
+  s.style.fontSize=n.value+'px';
+}
