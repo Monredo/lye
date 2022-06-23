5d3b54ed6d30a88ebe9d8223dc2c49742c4a4c48
Monredo <107286254+Monredo@users.noreply.github.com>
2022-06-23 01:13:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function changeSize(n){
   var s=document.getElementById('content');
   s.style.fontSize=n.value+'px';
 }
+
+functionBold() {
+  var bol=document.getElementById('content').style.fontWeight;
+  
+  if(bol=='normal') {
+    document.getElementById('content').style.fontWeight='bold';
+  }
+  
+  else {
+    document.getElementById('content').style.fontWeight='normal';
+  }
